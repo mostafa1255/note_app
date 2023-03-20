5b9ff831daf3f40f225148221ccf0368faf8ead0
Mostafa <nonatefa11@gmail.com>
2023-03-20 03:41:33 +0200

    Add border Radius in Buttom sheet

--- lib/view/notes_view.dart
+++ lib/view/notes_view.dart
@@ -13,6 +13,8 @@ class NotesView extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showModalBottomSheet(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16)),
               context: context,
               builder: (context) {
                 return butomSheet();
@@ -24,4 +26,3 @@ class NotesView extends StatelessWidget {
     );
   }
 }
-
